2a7084f8f099e7e738724c2959b14c254ff7c4df
Jeff Ragland <49291226+jragl@users.noreply.github.com>
2019-11-20 01:53:12 -0600

    Add files via upload
    Added carousel dots and made code more responsive. Still need to correct javascript as it only displays one dot.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var slideIndex = 1;
 showDivs(slideIndex);
+showSlides(slideIndex);
+
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
@@ -24,4 +26,34 @@ function openNav() {
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
+}
+
+// ---------- Carousel navigation dots ---------- //
+// showSlides(slideIndex);
+
+// remarked out
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+// end remarked out
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("visualVersions");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
