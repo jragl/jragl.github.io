94a30e3db11addeea4312188f663d871b0fface8
Jeff Ragland <49291226+jragl@users.noreply.github.com>
2020-02-25 23:25:31 -0600

    Add files via upload
    Updates to portfolio site include some PetSet images and some CSS. Still need to work on scaling in BusyBus case study and add more Cumulus info.

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function goback() {
+//     window.history.back();
+// }
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+document.getElementById('arrow-left').addEventListener('click', () => {
+  window.history.back();
+});
+
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function goback() {
-    window.history.back();
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showDivs(n) {
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("visualVersions");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length};
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-    // visualVersionsDots[i].className = visualVersionsDots[i].className.replace(" active", "");
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  // x[slideIndex-1].style.display = "flex";
-  // visualVersionsDots[slideIndex-1].className += " active";
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("visualVersions");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length};
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//     // visualVersionsDots[i].className = visualVersionsDots[i].className.replace(" active", "");
+//   }
+//   // x[slideIndex-1].style.display = "flex";
+//   // visualVersionsDots[slideIndex-1].className += " active";
+// }
 
 /* Open when someone clicks on the span element */
 function openNav() {
